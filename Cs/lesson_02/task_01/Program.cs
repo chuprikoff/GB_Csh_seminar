22e23538c0c5c67cc7c0cb4abeb6b6f1e16f4c77
chuprikoff <chuprikoff@yandex.ru>
2023-01-02 00:43:42 +0300

    added task1

---
+++ Cs/lesson_02/task_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. Обязательна проверка на ввод чисел
+// больше/меньше 3-х знаков
+
+
+Console.Write("Введите з-х значное число: ");
+int UserNum=System.Convert.ToInt32(Console.ReadLine());;
+
+
+Console.WriteLine(myFunc(UserNum));
+string myFunc(int a){
+  while (((UserNum/100)<1)||((UserNum/100)>=10))
+  {
+    Console.WriteLine("Вы ввели некорректное значение!!!");
+    Console.Write("Введите з-х значное число: ");
+    UserNum = System.Convert.ToInt32(Console.ReadLine());
+    myFunc(UserNum);
+  }
+
+  int res=(UserNum/10)%10;
+  string myFuncStr=($"{UserNum} -> {res}");
+  return myFuncStr;
+}
+
