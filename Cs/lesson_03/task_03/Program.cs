c3c7fda67effc44478002281679f279a4541dbac
chuprikoff <chuprikoff@yandex.ru>
2023-01-02 21:20:52 +0300

    added task3

--- Cs/lesson_03/task_03/Program.cs
+++ Cs/lesson_03/task_03/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и 
 // выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое число: ");
+int user_N = System.Convert.ToInt32(Console.ReadLine());
+int[] num_arr = new int[user_N];
+for(int i = 0; i < num_arr.Length; i++)
+{
+  num_arr[i] = System.Convert.ToInt32(Math.Pow((i+1),3));
+}
+string result_str = String.Join(", ", num_arr);
+Console.WriteLine($"{user_N} -> {result_str}");
