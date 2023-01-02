1b4f19b6267660bb11d41581c5cf9472d665d260
chuprikoff <chuprikoff@yandex.ru>
2023-01-02 20:23:56 +0300

    added task2

---
+++ Cs/lesson_03/task_01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// Проверка на пятизначность обязательна
+
+bool is_Polyndrom(string str){
+  char[] reverse_arr = str.ToCharArray();
+  Array.Reverse(reverse_arr);
+  string reverse_str = new string(reverse_arr);
+  bool result = (str == reverse_str);
+  return result;
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+string userInput = Console.ReadLine();
+string res = "нет";
+Console.WriteLine(myFunc(userInput));
+
+string myFunc(string str){
+  while (userInput.Length != 5)
+  {
+    Console.WriteLine("Вы ввели некорректное значение!!!");
+    Console.Write("Введите пятизначное число: ");
+    userInput = Console.ReadLine();
+    myFunc(userInput);
+  }
+
+  if(is_Polyndrom(userInput)){
+    res = "да";
+  }
+  string myFuncStr=($"{userInput} -> {res}");
+  return myFuncStr;
+}
+
---
+++ Cs/lesson_03/task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 3D пространстве.
+
+
+Console.Write("Введите координаты первой точки по оси X: ");
+double x1 =System.Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты первой точки по оси Y: ");
+double y1 =System.Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты первой точки по оси Z: ");
+double z1 =System.Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты второй точки по оси X: ");
+double x2 =System.Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты второй точки по оси Y: ");
+double y2 =System.Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты второй точки по оси Z: ");
+double z2 =System.Convert.ToDouble(Console.ReadLine());
+
+double distance(double x1, double y1, double z1, double x2, double y2, double z2){
+ double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+ result = Math.Round(result, 4);
+
+ return result;
+}
+
+Console.WriteLine($"A({x1}; {y1}; {z1}); B({x2}; {y2}; {z2}) -> {distance(x1, y1, z1, x2, y2, z2)}");
