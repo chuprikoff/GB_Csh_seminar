0e99e1cbf709ff6aaf62286179516d2a641b95a2
chuprikoff <chuprikoff@yandex.ru>
2023-01-02 11:18:38 +0300

    added task2

---
+++ Cs/lesson_02/task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет. Берем числа до 100000
+
+
+Console.WriteLine("Введите число: ");
+string UserInput = Console.ReadLine();
+
+if (UserInput.Length<3)
+{
+  Console.WriteLine($"{UserInput} -> третьей цифры нет");
+}
+else
+{
+  Console.WriteLine($"{UserInput} -> {UserInput[2]}");
+}
