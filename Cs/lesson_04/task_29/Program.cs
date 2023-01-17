6abef8919a151b8b1149395ad7ece3001b4751c4
chuprikoff <chuprikoff@yandex.ru>
2023-01-17 20:55:05 +0300

    added task29

--- Cs/lesson_04/task_29/Program.cs
+++ Cs/lesson_04/task_29/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. (числа берете любые)
+
+randArr();
+
+//================method====================
+
+void randArr(){
+  Random randNum = new Random();
+  int[] res = new int[8];
+  for (int i = 0; i < res.Length; i++)
+  {
+    res[i] = randNum.Next(0, 100);
+    Console.Write($"{res[i]}, ");
+  }
+}
