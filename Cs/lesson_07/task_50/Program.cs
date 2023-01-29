0bab774d0ad5d4765cb366a4e4303821b3db3402
chuprikoff <chuprikoff@yandex.ru>
2023-01-29 17:36:36 +0300

    finished lesson_07

---
+++ Cs/lesson_07/task_50/Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая на вход принимает число, проверяя есть ли такое 
+// число в двумерном массиве и возвращает сообщение о том, что оно найдено или же 
+// указание, что такого элемента нет.
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+//=================================================================
+
+Console.Write("Введите число: ");
+int userNumber = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(4, 4, -20, 20);
+PrintArray(array);
+ElemIsArray(array);
+
+
+
+
+
+
+//===================methods===============
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void ElemIsArray(int[,] array)
+{
+    bool res = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == userNumber)
+            {
+                res = true;
+                break;
+            };
+        }
+    }
+    if (res) Console.Write($"{userNumber} => число найдено в массиве!");
+    else Console.Write($"{userNumber} => такого числа в массиве нет!");
+
+
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ Cs/lesson_07/task_52/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, -99, 100);
+PrintArray(array);
+Console.WriteLine();
+MyPrintArray(array);
+
+// -------------Метод----------------------------
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 0 && array[i, j] < 10) Console.Write($"  {array[i, j]} |");
+            else if
+                ((array[i, j] < 0 && array[i, j] > -10)
+                || (array[i, j] > 9 && array[i, j] < 100))
+            {
+                Console.Write($" {array[i, j]} |");
+            }
+            else Console.Write($"{array[i, j]} |");
+        }
+        Console.WriteLine();
+    }
+}
+void MyPrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            if (array[j, i] >= 0 && array[j, i] < 10) Console.Write($"  {array[j, i]} |");
+            else if
+                ((array[j, i] < 0 && array[j, i] > -10)
+                || (array[j, i] > 9 && array[j, i] < 100))
+            {
+                Console.Write($" {array[j, i]} |");
+            }
+            else Console.Write($"{array[j, i]} |");
+            sum += array[j, i];
+        }
+        Console.WriteLine($" -> {Math.Round((sum / rows), 2)}");
+    }
+}
+
