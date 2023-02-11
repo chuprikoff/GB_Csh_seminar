e7d5bf63da61c9e3db756593b153b8a3a09734d6
chuprikoff <chuprikoff@yandex.ru>
2023-02-11 20:02:48 +0300

    finished lesson_09

---
+++ Cs/lesson_09/task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Выполнить с помощью рекурсии.Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введите натуральное число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N, 1));
+
+//====================Methods================
+
+
+string PrintNumbers(int start, int finish)
+{
+    if (start == finish)
+    {
+        return start.ToString();
+    }
+    return (start + " " + PrintNumbers(start - 1, finish));
+}
---
+++ Cs/lesson_09/task_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Выполнить с помощью рекурсии.Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите натуральное число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите натуральное число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+if (N > M)
+{
+    (N, M) = (M, N);
+}
+Console.WriteLine($"M = {M}, N = {N}| => {myFunc(N)}");
+
+//====================Methods=============
+
+int myFunc(int N)
+{
+    while (N < M)
+    {
+        return N + myFunc(N + 1);
+    }
+    return N;
+}
